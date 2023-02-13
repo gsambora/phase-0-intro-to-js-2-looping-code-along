8f54b7394a37b9f38c06d9096cf7eb598aa8fd78
Hana <grace.sambora@gmail.com>
2023-02-12 22:47:55 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    // console.log(`I'm ${age} years old. Happy birthday to me!`);   
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+function writeCards(names, event) {
+    let writtenCards = [];
+    for (let i=0; i < names.length; i++) {
+        writtenCards[i] = "Thank you, " + names[i] + ", for the wonderful " + event + " gift!";
+    }
+
+    return writtenCards;
+}
+
+function countDown(number) {
+    for(let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+countDown(10);
